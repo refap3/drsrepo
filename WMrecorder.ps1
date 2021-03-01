e827fe010ca724a7a5605d038a1291792aee7795
refap3 <refap3@gmail.com>
2021-03-01 13:11:49 +0100

    re load schedule file if enough time until next recording

--- WMrecorder.ps1
+++ WMrecorder.ps1
@@ -1,45 +1,52 @@
 # WM recorder in powershell ...
 #
-$BuildNumber=1.12
+$BuildNumber=1.15
 $BrowserName = "opera"
-while ($true)
+while ($true) # use another outer loop for break inside inner loop 
 {
-  $wmrecs =Import-Csv c:\temp\scheduled.csv -Delimiter ";" | `
-  Select @{Name="RecordingTime";Expression={(Get-date $_.RecordingTime)}}, @{Name="EndTime";Expression={(get-date $_.EndTime)}}, @{Name="Length";Expression={[int32]$_.Length}}, Filename, Link  | where Recordingtime -gt ((get-date).AddSeconds(-30)) | sort Recordingtime 
-#  if ($wmrecs.Length -gt 0 ) {echo "$($wmrecs.length) recording(s) ... Next @ $($wmrecs[0].RecordingTime) to $($wmrecs[0].EndTime.Hour.ToString('00')):$($wmrecs[0].EndTime.Minute.ToString('00')) - $($wmrecs[0].FileName)" }
-  
-  # find the 1st recordable entry  and display it, focc off iff nothing ....
-  if ($wmrecs -ne $null) 
+  while ($true)
   {
-    $nextRec = $wmrecs[0]
-    $ssecs=(($nextRec.RecordingTime) - (get-date)).totalseconds
-    $smins=$ssecs/60
-    echo "V: $BuildNumber-will record this: $nextRec in $($smins.tostring('0')) minutes."
- 
-    if ($ssecs -gt 0 ) {sleep  $ssecs } # sleep until start time 
-    echo "recording NOW: $($nextRec.RecordingTime) to $($nextRec.EndTime.Hour.ToString('00')):$($nextRec.EndTime.Minute.ToString('00')) - $($nextRec.FileName)"
-    start $nextRec.Link
-    sleep (($nextRec.EndTime) - (get-date)).totalseconds # sleep until end time 
-      
-    ps $BrowserName|kill -ErrorAction SilentlyContinue
-    sleep 3
-
-    # rename out file 
-    $files=ls c:\temp\*.mp3|sort LastWriteTime -Descending
-    if ($files -ne $null)
+    "$BuildNumber :refresh SCHEDULE file "
+    $wmrecs =Import-Csv c:\temp\scheduled.csv -Delimiter ";" | `
+    Select @{Name="RecordingTime";Expression={(Get-date $_.RecordingTime)}}, @{Name="EndTime";Expression={(get-date $_.EndTime)}}, @{Name="Length";Expression={[int32]$_.Length}}, Filename, Link  | where Recordingtime -gt ((get-date).AddSeconds(-30)) | sort Recordingtime 
+    #  if ($wmrecs.Length -gt 0 ) {echo "$($wmrecs.length) recording(s) ... Next @ $($wmrecs[0].RecordingTime) to $($wmrecs[0].EndTime.Hour.ToString('00')):$($wmrecs[0].EndTime.Minute.ToString('00')) - $($wmrecs[0].FileName)" }
+  
+    # find the 1st recordable entry  and display it, focc off iff nothing ....
+    if ($wmrecs -ne $null) 
     {
-      $lastf=$files[0]
-      Rename-Item -Path $lastf.FullName -NewName "$($nextRec.FileName).mp3"
-    }
-    else {echo "No output mp3 file found ... !"}
+      $nextRec = $wmrecs[0]
+      $ssecs=(($nextRec.RecordingTime) - (get-date)).totalseconds
+      $smins=$ssecs/60
     
-    sleep 2
+      # if enough time until next recording sleep and re-read schedule again 
+      if ($smins -gt 10) {echo "$($smins.tostring('0')) mins to go for $($nextRec.Filename) ..." ; sleep 60 ; break }
+    
+      echo "V: $BuildNumber-will record this: $nextRec in $($smins.tostring('0')) minutes."
+ 
+      if ($ssecs -gt 0 ) {sleep  $ssecs } # sleep until start time 
+      echo "recording NOW: $($nextRec.RecordingTime) to $($nextRec.EndTime.Hour.ToString('00')):$($nextRec.EndTime.Minute.ToString('00')) - $($nextRec.FileName)"
+      start $nextRec.Link
+      sleep (($nextRec.EndTime) - (get-date)).totalseconds # sleep until end time 
+      
+      ps $BrowserName|kill -ErrorAction SilentlyContinue
+      sleep 3
+
+      # rename out file 
+      $files=ls c:\temp\*.mp3|sort LastWriteTime -Descending
+      if ($files -ne $null)
+      {
+        $lastf=$files[0]
+        Rename-Item -Path $lastf.FullName -NewName "$($nextRec.FileName).mp3"
+      }
+      else {echo "No output mp3 file found ... !"}
+    
+      sleep 2
+    }
+    else 
+    {
+      echo "Nothing found ... sleep and retry!"
+      sleep 5
+    }
   }
-  else 
-  {
-    echo "Nothing found ... sleep and retry!"
-    sleep 5
-  }
-}
-  
+} 
 
