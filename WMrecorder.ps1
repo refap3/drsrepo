c6ae48eaacae0cdb91544ae730f40d4153199a3d
refap3 <refap3@gmail.com>
2021-03-02 10:18:54 +0100

    shorten recording time if next sendung start immediatly after curren

--- WMrecorder.ps1
+++ WMrecorder.ps1
@@ -1,8 +1,16 @@
 # WM recorder in powershell ...
 #
 $BuildNumber=1.19
-$secondsBefore=5 # adjust recording time range ...
+# Setup adjust recording time range ...
+# Grace: accept if record start is before now - grace
+# Before: Begin recording after before seconds, ie. 5: 5 secs later 
+# After: Time to record AFTER EndTime
+
+$secondsGrace = -30
+$secondsBefore=5 
 $secondsAfter=30
+
+
 $BrowserName = "opera"
 while ($true) # use another outer loop for break inside inner loop 
 {
@@ -10,7 +18,7 @@ while ($true) # use another outer loop for break inside inner loop
   {
     "$BuildNumber :refresh SCHEDULE file "
     $wmrecs =Import-Csv c:\temp\scheduled.csv -Delimiter ";" | `
-    Select @{Name="RecordingTime";Expression={(Get-date $_.RecordingTime)}}, @{Name="EndTime";Expression={(get-date $_.EndTime)}}, @{Name="Length";Expression={[int32]$_.Length}}, Filename, Link  | where Recordingtime -gt ((get-date).AddSeconds(-30)) | sort Recordingtime 
+    Select @{Name="RecordingTime";Expression={(Get-date $_.RecordingTime)}}, @{Name="EndTime";Expression={(get-date $_.EndTime)}}, @{Name="Length";Expression={[int32]$_.Length}}, Filename, Link  | where Recordingtime -gt ((get-date).AddSeconds($secondsGrace)) | sort Recordingtime 
     #  if ($wmrecs.Length -gt 0 ) {echo "$($wmrecs.length) recording(s) ... Next @ $($wmrecs[0].RecordingTime) to $($wmrecs[0].EndTime.Hour.ToString('00')):$($wmrecs[0].EndTime.Minute.ToString('00')) - $($wmrecs[0].FileName)" }
   
     # find the 1st recordable entry  and display it, focc off iff nothing ....
@@ -26,6 +34,8 @@ while ($true) # use another outer loop for break inside inner loop
       echo "V: $BuildNumber-will record this: $nextRec in $($smins.tostring('0')) minutes."
  
       if ($ssecs -gt 0 ) {sleep  $ssecs } # sleep until start time 
+      # compute maximum sec after recording ... Problem if next Recording starts @ this.EndTime ....
+      if ($wmrecs.length -gt 1) { if ($nextRec.EndTime -eq $wmrecs[1].RecordingTime){}$secondsAfter=0}
       echo "recording NOW: $($nextRec.RecordingTime) to $($nextRec.EndTime.Hour.ToString('00')):$($nextRec.EndTime.Minute.ToString('00')) - $($nextRec.FileName)"
       start $nextRec.Link
       sleep ((($nextRec.EndTime) - (get-date)).totalseconds + $secondsAfter) # sleep until end time 
