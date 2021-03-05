f8b03c45a6c010ca91ed80d88d32f3e879fbc1b1
refap3 <refap3@gmail.com>
2021-03-05 11:22:59 +0100

    made settings changes permanent -- dunno y i had to add extra code WTF

---
+++ schedWatcher/Settings2.vb
@@ -0,0 +1,16 @@
+﻿
+Imports System.ComponentModel
+
+Namespace My
+
+    'This class allows you to handle specific events on the settings class:
+    ' The SettingChanging event is raised before a setting's value is changed.
+    ' The PropertyChanged event is raised after a setting's value is changed.
+    ' The SettingsLoaded event is raised after the setting values are loaded.
+    ' The SettingsSaving event is raised before the setting values are saved.
+    Partial Friend NotInheritable Class MySettings
+        Private Sub MySettings_PropertyChanged(sender As Object, e As PropertyChangedEventArgs) Handles Me.PropertyChanged
+            My.Settings.Save()
+        End Sub
+    End Class
+End Namespace
